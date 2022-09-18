0b280e921d5f8c84976be06d2537c98842014a16
Pedro Lorenzo Rosario <63958751+PALR-1216@users.noreply.github.com>
2022-09-17 21:38:42 -0400

    Update BubbleSort.js

--- BubbleSort.js
+++ BubbleSort.js
@@ -1,21 +1,19 @@
-class Solution
-{
-   //Function to sort the array using bubble sort algorithm.
-   //find a way to check values > 
-   bubbleSort(arr,n){
-      
-      for(let i =0; i < arr.length; i++){
-          for(let j =0; j < arr.length - i - 1; j++) {
-              if(arr[j] > arr[j + 1]) {
-                  let temp = arr[j];
-                  arr[j] = arr[j + 1]
-                  arr[j + 1] = temp
-              }
-          }
+
+function BubbleSort(arr) {
+  //make a nested for loop
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      //check if index i > index j:
+      if (arr[j] > arr[j + 1]) {
+        //make a temporal variable and store the greater value
+        let temp = arr[j];
+        //swap the spot with the greater index value for the smaller value
+        arr[j] = arr[j + 1];
+        //set the greater index value where the smaller value was located
+        arr[j + 1] = temp;
       }
-       
-    //code here
-    console.log(arr)
-   }
-    
+    }
+  }
+
+  console.log(arr);
 }
